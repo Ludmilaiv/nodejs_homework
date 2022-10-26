9114591450e7c4af2d5aa6e22b19b0ac2fa716cc
Людмила <56251614+Ludmilaiv@users.noreply.github.com>
2022-10-26 22:55:56 +0700

    lesson 1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("hello");
+import colors from "colors";
+import readline from 'readline';
+
+const rl = readline.createInterface({ 
+  input: process.stdin, 
+  output: process.stdout
+});
+
+const isPrime = (number) => {
+  const max = Math.sqrt(number);
+  let prime = true;
+  for (let i = 2; i <= max; i++) {
+    if (number % i === 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
+
+rl.question("Введите начало диапазона ", (beginNumber) => {
+  rl.question("Введите начало диапазона ", (endNumber) => {
+    if (!Number(beginNumber) || !Number(endNumber)){
+      throw "Один из аргументов не является числом!";
+    }
+    let count = 0;
+    for (let i = beginNumber; i <= endNumber; i++) {
+      if (isPrime(i)) {
+        count++;
+        count % 3 === 0 ? console.log(colors.red(i)) : 
+        count % 3 === 1 ? console.log(colors.green(i)) :
+        console.log(colors.yellow(i));
+      }
+    }
+    if (count === 0) {
+      console.log(colors.red("Простых чисел в диапазоне нет"))
+    }
+    rl.close();
+  });
+});
