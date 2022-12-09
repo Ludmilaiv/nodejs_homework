e247ff85769dbacc1b4bb679d0945a8bfb593625
Людмила <56251614+Ludmilaiv@users.noreply.github.com>
2022-12-09 21:19:37 +0700

    lesson 2

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function getMonthLength(year, monthNumber)
-{
+class Time {
+  constructor(dateString) {
+    const [hours, date, month, year] = dateString.split("-");
+    this.hours = hours;
+    this.date = date;
+    this.month = month;
+    this.year = year;
+  }
+  show() {
+    console.log(`${this.hours}-${this.date}-${this.month}-${this.year}`);
+  }
+}
+
+const getMonthLength = (year, monthNumber) => {
   const monthLength = [31, 28, 31, 30, 31, 31, 30, 31, 30, 31];
   if (monthNumber != 2) {
     return monthLength[monthNumber - 1];
@@ -18,68 +30,91 @@ function getMonthLength(year, monthNumber)
   return 28;
 }
 
-class Time {
-  constructor(dateString) {
-    const [hours, date, month, year] = dateString.split("-");
-    console.log(dateString.split("-"));
-    this.hours = hours;
-    this.date = date;
-    this.month = month;
-    this.year = year;
-  }
-  show() {
-    console.log(`Осталось: ${this.hours}-${this.date}-${this.month}-${this.year}`);
-  }
-}
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+  setTimeout(resolve, ms);
+  });
+};
 
-const changeYear = time => {
-  if (time.year == 1) {
-    console.log("Все таймеры закончились");
-  } else {
+class Handler {
+  static changeYear(time) {
     time.year--;
     time.show();
-    setTimeout(() => changeHours(time), 1000);
   }
-}
 
-const changeMonth = time => {
-  if (time.month == 1) {
-    time.month = 12;
-    console.log("Таймеры дней, часов и месяцев закончились");
-    setTimeout(() => changeYear(time), 1000);
-  } else {
+  static changeMonth(time) {
     time.month--;
     time.show();
-    setTimeout(() => changeHours(time), 1000);
   }
-}
 
-const changeDate = time => {
-  if (time.date == 1) {
-    time.date = getMonthLength(time.year, time.month);
-    console.log("Таймеры часов и месяцев закончились");
-    setTimeout(() => changeMonth(time), 1000);
-  } else {
+  static changeDate(time) {
     time.date--;
     time.show();
-    setTimeout(() => changeHours(time), 1000);
+  }
+
+  static changeHours(time) {
+    time.hours--;
+    time.show();
+  }
+
+  static finishHours(time) {
+    time.hours = 23;
+    console.log("Таймер часов закончился");
+  }
+
+  static finishMonth(time) {
+    time.month = 12;
+    console.log("Таймер месяцев закончился");
+  }
+
+  static finishDate(time) {
+    time.date = getMonthLength(time.year, time.month);
+    console.log("Таймер дней закончился");
+  }
+
+  static finishYears(time) {
+    console.log("Таймер лет закончился");
   }
 }
 
-const changeHours = time => {
+class MyEmitter extends EventEmitter {};
+const emitterObject = new MyEmitter();
+
+emitterObject.on('changeYear', Handler.changeYear);
+emitterObject.on('changeMonth', Handler.changeMonth);
+emitterObject.on('changeDate', Handler.changeDate);
+emitterObject.on('changeHours', Handler.changeHours);
+emitterObject.on('finishYears', Handler.finishYears);
+emitterObject.on('finishMonth', Handler.finishMonth);
+emitterObject.on('finishDate', Handler.finishDate);
+emitterObject.on('finishHours', Handler.finishHours);
+
+ const timerTick = time => {
   if (time.hours == 1) {
-    time.hours = 23;
-    console.log("Таймер часов закончился");
-    setTimeout(() => changeDate(time), 1000);
+    delay(1000).then(emitterObject.emit('finishHours', time));
+    if (time.date == 1) {
+      delay(1000).then(emitterObject.emit('finishDate', time));
+      if (time.month == 1) {
+        delay(1000).then(emitterObject.emit('finishMonth', time));
+        if (time.year == 1) {
+          delay(1000).then(emitterObject.emit('finishYears', time));
+          return;
+        }
+        emitterObject.emit('changeYear', time);
+      } else {
+        emitterObject.emit('changeMonth', time);
+      } 
+    } else {
+      emitterObject.emit('changeDate', time);
+    }
   } else {
-    time.hours--;
-    time.show();
-    setTimeout(() => changeHours(time), 1000);
-  }
+    emitterObject.emit('changeHours', time);
+  }  
+  delay(1000).then(() => timerTick(time));
 }
 
 rl.question("Введите дату: ", (dateString) => {
   const time = new Time(dateString);
-  setTimeout(() => changeHours(time), 1000);
+  delay(1000).then(() => timerTick(time));
   rl.close();
 });
