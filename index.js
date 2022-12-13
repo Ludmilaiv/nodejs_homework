3ee82485dc137172e2ba7fef8807162542aecb91
Людмила <56251614+Ludmilaiv@users.noreply.github.com>
2022-12-13 23:45:35 +0700

    lesson 3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello");
+import readline from 'readline';
+import fs from 'fs';
+
+const rs = fs.createReadStream('./access_tmp.log', 'utf8');
+
+const rl = readline.createInterface({ 
+  input: rs, 
+  output: process.stdout,
+  terminal: false,
+});
+
+const searchIP_1 = '89.123.1.41';
+const searchIP_2 = '34.48.240.111';
+
+const main = async () => {
+  await fs.writeFile(`ip-${searchIP_1}`, '', err => {if (err) console.log(err)});
+  await fs.writeFile(`ip-${searchIP_2}`, '', err => {if (err) console.log(err)});
+  rl.on('line',(line) => {
+    if (line.indexOf(searchIP_1) == 0) {
+      fs.writeFile(`ip-${searchIP_1}`, line + '\n', { flag: 'a' }, err => {if (err) console.log(err)})
+    } else if (line.indexOf(searchIP_2) == 0) {
+      fs.writeFile(`ip-${searchIP_2}`, line + '\n', { flag: 'a' }, err => {if (err) console.log(err)})
+    }
+  });
+}
+
+main();
